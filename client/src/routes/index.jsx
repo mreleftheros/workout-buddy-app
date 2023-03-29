c2f6ccd42585ef0de928e2160c4f83e73e1c888f
mreleftheros <eleftheros_all@protonmail.com>
2023-03-29 05:03:36 +0300

    add hero img

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,34 +1,40 @@
 import { A } from "solid-start";
 import { useAuthContext } from "~/context/auth";
+import workout from "../assets/workout.svg";
 
 const index = () => {
   const { auth } = useAuthContext();
 
   return (
     <section className="hero">
-      <h1 class="hero-title">
-        Welcome to <strong>Workout Buddy App 👐</strong>
-      </h1>
-      <h2 class="hero-subtitle">
-        Organize and keep track of your workout sessions easily.
-      </h2>
-      <div className="hero-cta">
-        <Show
-          when={!auth()}
-          fallback={
-            <A class="hero-cta-btn active" href="/workouts">
-              View Workouts
+      <figure class="hero-figure">
+        <img class="hero-img" src={workout} width={400} />
+      </figure>
+      <div class="hero-details">
+        <h1 class="hero-title">
+          Welcome to <strong>Workout Buddy App 👐</strong>
+        </h1>
+        <h2 class="hero-subtitle">
+          Organize and keep track of your workout sessions easily.
+        </h2>
+        <div className="hero-cta">
+          <Show
+            when={!auth()}
+            fallback={
+              <A class="hero-cta-btn active" href="/workouts">
+                View Workouts
+              </A>
+            }
+          >
+            <A class="hero-cta-btn" href="/login">
+              Login
             </A>
-          }
-        >
-          <A class="hero-cta-btn" href="/login">
-            Login
-          </A>
-          <span>or</span>
-          <A class="hero-cta-btn" href="/signup">
-            Sign up
-          </A>
-        </Show>
+            <span>or</span>
+            <A class="hero-cta-btn" href="/signup">
+              Sign up
+            </A>
+          </Show>
+        </div>
       </div>
     </section>
   );
